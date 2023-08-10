8759ea6572eb464f519d9963f7260761be1ae139
Yash Jivani <yashyj1908@gamil.com>
2023-08-10 17:02:20 +0530

    1st Attempt

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,28 @@ package com.driver;
 
 public class Main {
 
+    public static void main(String[] args) {
+        Product p = new Product();
+
+        p.product(2,3);
+        p.product(2,3,4);
+        p.product(2.2,3.3);
+    }
+
+    static class Product{
+        public int product(int x, int y) {
+            return x*y;
+        }
+
+        public int product(int x, int y, int z) {
+            return x*y*z;
+        }
+
+        public double product(double x, double y) {
+            return x*y;
+        }
+
+    }
+
 }
+
